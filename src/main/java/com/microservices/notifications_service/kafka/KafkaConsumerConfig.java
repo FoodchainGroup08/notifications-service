df283c23b671aa2caac223ef6c1b9622ed9a5777
Zainab Omolola Fadeyi <70825458+Zainabfadeyi@users.noreply.github.com>
2026-05-19 21:14:46 +0100

    push

---
+++ src/main/java/com/microservices/notifications_service/kafka/KafkaConsumerConfig.java
@@ -0,0 +1,55 @@
+package com.microservices.notifications_service.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${KAFKA_API_KEY:}")
+    private String kafkaApiKey;
+
+    @Value("${KAFKA_API_SECRET:}")
+    private String kafkaApiSecret;
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "notifications-service-group");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        if (kafkaApiKey != null && !kafkaApiKey.isBlank()) {
+            config.put("security.protocol", "SASL_SSL");
+            config.put("sasl.mechanism", "PLAIN");
+            config.put("sasl.jaas.config",
+                    "org.apache.kafka.common.security.plain.PlainLoginModule required " +
+                    "username=\"" + kafkaApiKey + "\" " +
+                    "password=\"" + kafkaApiSecret + "\";");
+        }
+
+        return new DefaultKafkaConsumerFactory<>(config);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+}
